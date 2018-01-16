70eccc98513dbe5f1df2efc1da777d2b2e9c4ce9
Abdulsamet ŞAHİN <abdulsametsah@gmail.com>
2018-01-16 08:41:42 +0300

    ..

--- resources/views/mobil/ara.blade.php
+++ resources/views/mobil/ara.blade.php
@@ -6,9 +6,9 @@
 <div class="arama row">
     <div class="col col-6">
         <h5><i class="fa fa-tv"></i> Filmler</h5>
-        <div class="filmler row">
+        <div class="filmler">
             @foreach($filmler as $film)
-            <div class="col film">
+            <div class=" film">
                 <a href="{{ $film->url() }}">
                     <img style="height:auto" src="{{ $film->mini_poster() }}" alt="">
                     <div class="name">{{ $film->name(30) }} <span class="puan">{{ $film->puan }}</span></div>
@@ -19,9 +19,9 @@
     </div>
     <div class="col col-6">
         <h5><i class="fa fa-user-circle"></i> Oyuncular</h5>
-        <div class="filmler row">
+        <div class="filmler">
             @foreach($oyuncular as $oyuncu)
-            <div class="col film">
+            <div class=" film">
                 <a href="{{ $oyuncu->url() }}">
                     <img style="height:auto" src="{{ $oyuncu->photo2() }}" alt="">
                     <div class="name">{{ $oyuncu->name }} </div>
